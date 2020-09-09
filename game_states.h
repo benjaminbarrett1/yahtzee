9efcc4b0b64d0a9f08250e7d16cb0233af6a5e95
Ben Barrett <benjamin.barrett@bristol.ac.uk>
2020-09-09 19:35:18 +0100

    After noticing the time saving from precomputation of dice transitions I've decided to work in python instead.

--- game_states.h
+++
@@ -1,8 +0,0 @@
-#define UNK_VALUE -1 /* impossible value */
-#define ILLEGAL_CAT -1
-
-float *init_values();
-void export_values(float *values, FILE *fileptr);
-float *import_files(float *values, FILE *fileptr);
-int state_index(int upperscore, int opencats);
-
